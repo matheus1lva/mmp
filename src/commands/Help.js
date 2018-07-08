3fea396a4d91520fd876b63b3a1361616ed51a8f
PlayMa256 <matheus.g.silva@hotmail.com>
2018-07-08 19:09:46 +0000

    chore: add log utility

--- src/commands/Help.js
+++ src/commands/Help.js
@@ -2,17 +2,19 @@
 import type { Command } from "./Command";
 import type { CLI, CommandList } from "../types";
 import { getCommands } from "./index";
+import {Log} from '../utils/log';
 
 export default class Help implements Command {
 	run(cli: CLI): any {
 		const availableCommands: CommandList = getCommands();
+
 		// if we got here, it was asked a help of a certain command
 		const [_, target] = cli.input; //eslint-disable-line
 
 		if (!availableCommands[target]) {
-			throw new Error(`Help for command ${target} not found`);
+			return Log.error(`Help for command ${target} not found`);
 		}
-		console.log(availableCommands[target].help());
+		return Log.success(availableCommands[target].help());
 	}
 
 	help() {
